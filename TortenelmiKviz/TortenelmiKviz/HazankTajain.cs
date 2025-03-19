1eeed6f22c7550cf8dc30c7e07990bff81de07fa
milan <adammilan95@gmail.com>
2025-03-19 10:43:39 +0100

    Ran code cleanup

--- TortenelmiKviz/TortenelmiKviz/HazankTajain.cs
+++ TortenelmiKviz/TortenelmiKviz/HazankTajain.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using static System.Formats.Asn1.AsnWriter;
-
-namespace TortenelmiKviz
+﻿namespace TortenelmiKviz
 {
     public partial class HazankTajain : Form
     {
